ed342468d0169f305a19707f0d47d0b882487586
Jam Bees <jam@jamandbees.net>
2014-06-08 11:42:30 -0600

    Latest schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140608154449) do
+ActiveRecord::Schema.define(version: 20140608163815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140608154449) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "to_do_list_id"
+    t.boolean  "deleted",       default: false
   end
 
   create_table "to_do_lists", force: true do |t|
