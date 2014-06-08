ccd5a5df8d57cce0340c9519bdf71de027b9620b
Jam Bees <jam@jamandbees.net>
2014-06-08 14:48:27 -0600

    issue #7 changed willExpireAt to be snake_case.

--- db/migrate/20140608204216_add_will_expire_at_to_to_do_items.rb
+++ db/migrate/20140608204216_add_will_expire_at_to_to_do_items.rb
@@ -1,5 +1,5 @@
 class AddWillExpireAtToToDoItems < ActiveRecord::Migration
   def change
-    add_column :do_items, :willExpireAt, :datetime
+    add_column :to_do_items, :will_expire_at, :datetime
   end
 end
