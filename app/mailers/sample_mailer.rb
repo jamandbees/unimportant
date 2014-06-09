8b8533cc38370db742d208857eb97a38c77a9688
Jam Bees <jam@jamandbees.net>
2014-06-08 21:03:22 -0600

    Deleted sample mailer, my little test email thingy.

--- app/mailers/sample_mailer.rb
+++
@@ -1,7 +0,0 @@
-class SampleMailer < ActionMailer::Base
-  def roflcopter
-    mail :subject => "Mandrill rides the Rails!",
-         :to      => "oscartheduck@gmail.com",
-         :from    => "mailer@importance.jamandbees.net"
-  end
-end
